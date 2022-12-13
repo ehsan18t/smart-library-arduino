7cff5430388629627750198e0bee5cb39c03d116
Ahsan Khan <30434801+ehsan18t@users.noreply.github.com>
2022-12-13 16:45:32 +0600

    fixed led not lit correctly

--- fingerprint_module/fingerprint_module.ino
+++ fingerprint_module/fingerprint_module.ino
@@ -27,6 +27,8 @@ uint8_t led3 = 10;
 void setup()
 {
   pinMode(led1, OUTPUT);
+  pinMode(led2, OUTPUT);
+  pinMode(led3, OUTPUT);
   matched = 0;
   Serial.begin(9600);
   while (!Serial);  // For Yun/Leo/Micro/Zero/...
